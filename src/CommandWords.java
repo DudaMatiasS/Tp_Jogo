1335ca5c6008b8c96a4671659f36157302811cf8
DudaMatiasS <membsimoess@gmail.com>
2023-12-12 21:12:14 -0300

    new feat: I implemented the items command

--- src/CommandWords.java
+++ src/CommandWords.java
@@ -1,6 +1,6 @@
 public class CommandWords {
     //essa classe reconhece os comandos digitados, ou seja, ela é responsável quando for adicionar um comando novo, o parser so ira interpretar o comando
-    private static final String[] validCommands = {"go", "quit", "help", "look","back","take","drop"};
+    private static final String[] validCommands = {"go", "quit", "help", "look","back","take","drop","items"};
     public CommandWords(){
     }
     public boolean isCommand(String commandAvaliable) {
